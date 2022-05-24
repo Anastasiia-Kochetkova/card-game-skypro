38bf333b145f0a20426bf7211413732ae474cc20
Anastasiia_Kochetkova <nastasiya534@gmail.com>
2022-05-24 11:37:12 +0300

    Add pair card generation

---
+++ card-collection.js
@@ -0,0 +1,148 @@
+const cardsData = [
+    {
+        name: "1",
+        image: "./cards/1.jpg",
+    },
+    {
+        name: "2",
+        image: "./cards/2.jpg",
+    },
+    {
+        name: "3",
+        image: "./cards/3.jpg",
+    },
+    {
+        name: "4",
+        image: "./cards/4.jpg",
+    },
+    {
+        name: "5",
+        image: "./cards/5.jpg",
+    },
+    {
+        name: "6",
+        image: "./cards/6.jpg",
+    },
+    {
+        name: "7",
+        image: "./cards/7.jpg",
+    },
+    {
+        name: "8",
+        image: "./cards/8.jpg",
+    },
+    {
+        name: "9",
+        image: "./cards/9.jpg",
+    },
+    {
+        name: "10",
+        image: "./cards/10.jpg",
+    },
+    {
+        name: "11",
+        image: "./cards/11.jpg",
+    },
+    {
+        name: "12",
+        image: "./cards/12.jpg",
+    },
+    {
+        name: "13",
+        image: "./cards/13.jpg",
+    },
+    {
+        name: "14",
+        image: "./cards/14.jpg",
+    },
+    {
+        name: "15",
+        image: "./cards/15.jpg",
+    },
+    {
+        name: "16",
+        image: "./cards/16.jpg",
+    },
+    {
+        name: "17",
+        image: "./cards/17.jpg",
+    },
+    {
+        name: "18",
+        image: "./cards/18.jpg",
+    },
+    {
+        name: "19",
+        image: "./cards/19.jpg",
+    },
+    {
+        name: "20",
+        image: "./cards/20.jpg",
+    },
+    {
+        name: "21",
+        image: "./cards/21.jpg",
+    },
+    {
+        name: "22",
+        image: "./cards/22.jpg",
+    },
+    {
+        name: "23",
+        image: "./cards/23.jpg",
+    },
+    {
+        name: "24",
+        image: "./cards/24.jpg",
+    },
+    {
+        name: "25",
+        image: "./cards/25.jpg",
+    },
+    {
+        name: "26",
+        image: "./cards/26.jpg",
+    },
+    {
+        name: "27",
+        image: "./cards/27.jpg",
+    },
+    {
+        name: "28",
+        image: "./cards/28.jpg",
+    },
+    {
+        name: "29",
+        image: "./cards/29.jpg",
+    },
+    {
+        name: "30",
+        image: "./cards/30.jpg",
+    },
+    {
+        name: "31",
+        image: "./cards/31.jpg",
+    },
+    {
+        name: "32",
+        image: "./cards/32.jpg",
+    },
+    {
+        name: "33",
+        image: "./cards/33.jpg",
+    },
+    {
+        name: "34",
+        image: "./cards/34.jpg",
+    },
+    {
+        name: "35",
+        image: "./cards/35.jpg",
+    },
+    {
+        name: "36",
+        image: "./cards/36.jpg",
+    },
+];
+
+module.exports = { cardsData };
